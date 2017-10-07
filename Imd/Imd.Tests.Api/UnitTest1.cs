539b372ace5d75b1e55f1d047bbf66cbac07a8a2
Michal Turzynski <Michal.Turzynski@pythagoras.co.uk>
2017-10-07 13:36:08 +0100

    initial solution structure

---
+++ Imd/Imd.Tests.Api/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Imd.Tests.Api
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
